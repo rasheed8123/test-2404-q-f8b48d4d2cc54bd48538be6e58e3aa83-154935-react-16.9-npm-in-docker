76b90a5929201274f0e7ebf32207d28c73ebd9dd
Pankaj Kandpal <pkandpal28@gmail.com>
2022-05-21 10:05:52 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect,useState} from "react"
+import react from 'react';
 function App() {
+  const [data,setData] = useState([])
+  const [year,setYear] = useState("1960")
+  const [name,setName] = useState([])
+  const [tenData,setTen] =useState([])
+useEffect(()=>{
+  const getdata=async()=>{
+    try{
+      let res= await fetch("https://codejudge-question-artifacts.s3.ap-south-1.amazonaws.com/poplution-countries-yearwise.json")
+      let data = await res.json();
+      setData(data)
+      // data.sort((a,b)=>(b.Value-a.Value))
+      const filteredData = data.filter((e)=>{
+          return e.Year==year
+      })
+      let ten=[];
+      for(let i=0;i<10;i++)
+      {
+        ten[i]=filteredData[i];
+      }
+      setTen(ten);
+      setTimeout(()=>{
+        data.sort((a,b)=>(b.Value-a.Value))
+      const filteredData = data.filter((e)=>{
+        return e.Year==year
+    })
+    let ten=[];
+    for(let i=0;i<10;i++)
+    {
+      ten[i]=filteredData[i];
+    }
+    setTen(ten);
+      },100)
+    }
+    catch(err){
+
+    }
+  } 
+  getdata()
+},[])
+function handleChange(e){
+ 
+}
+
+  console.log(tenData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+       <select className='chart-select' onChange={handleChange}>
+        {data.map((e)=>(
+          <option value={e.Year}>{e.Year}</option>
+        ))}
+       </select>
+     </div>
+          <div className='bar-chart' style={{display:"flex"}}>
+              <div>
+                {tenData.map((e)=>(
+                  <div>{e['Country Name']}</div>
+                ))}
+              </div>
+              <div></div>
+              <div>
+              {tenData.map((e)=>(
+                  <div>{e['Value']}</div>
+                ))}
+              </div>
+          </div>
     </div>
   );
 }
